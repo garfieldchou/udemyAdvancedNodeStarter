734547bb791ae13a83a5230e7435baaf31a228f9
garfield <cmgarfield@gmail.com>
2018-06-17 22:59:38 +0800

    Lec.55, Unique Keys

--- services/cache.js
+++ services/cache.js
@@ -5,5 +5,8 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function () {
   console.log('IM ABOUT TO RUN A QUERY');
 
+  console.log(this.getQuery());
+  console.log(this.mongooseCollection.name);
+
   return exec.apply(this, arguments);
 };
