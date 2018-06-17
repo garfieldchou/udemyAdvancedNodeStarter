7351746ce61a144bbe45071c60af8e2a5814ac04
garfield <cmgarfield@gmail.com>
2018-06-17 09:34:47 +0800

    Lec.37, Key Customization
    https://mlab.com

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://gc:p-l1339a@ds153980.mlab.com:53980/blog_dev',
   cookieKey: '123123123'
 };
