a1521016acae620f99d37e2c5f158813f893ad36
garfield <cmgarfield@gmail.com>
2018-06-18 08:57:34 +0800

    Lec.56, Key Creation

--- services/cache.js
+++ services/cache.js
@@ -5,8 +5,11 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function () {
   console.log('IM ABOUT TO RUN A QUERY');
 
-  console.log(this.getQuery());
-  console.log(this.mongooseCollection.name);
+  const key = Object.assign({}, this.getQuery(),{
+    collection: this.mongooseCollection.name
+  });
+
+  console.log(key);
 
   return exec.apply(this, arguments);
 };
