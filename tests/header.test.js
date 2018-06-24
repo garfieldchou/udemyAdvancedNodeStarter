94d573023321ccab4a02338301875405162035ad
garfield <cmgarfield@gmail.com>
2018-06-24 15:23:09 +0800

    Lec.77, Browser Termination

--- tests/header.test.js
+++ tests/header.test.js
@@ -10,6 +10,10 @@ beforeEach(async () => {
   await page.goto('localhost:3000');
 });
 
+afterEach(async () => {
+  await browser.close();
+});
+
 test('We can launch a browser', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
