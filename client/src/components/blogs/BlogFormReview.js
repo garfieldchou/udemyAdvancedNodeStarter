4f77ca86bae635ea2d4080511f8b229134f63132
garfield <cmgarfield@gmail.com>
2018-07-30 22:55:56 +0800

    Lec.147, Recording Image Files

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -46,12 +46,11 @@ class BlogFormReview extends Component {
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file, history);
   }
 
   onFileChange(event) {
-    this.setState({ file: event.target.files });
-    console.log(event.target.files);
+    this.setState({ file: event.target.files[0] });
   }
 
   render() {
