03c65d6872518c573efbcc0a3c8f070d26d4a1d5
garfield <cmgarfield@gmail.com>
2018-07-31 09:51:19 +0800

    Lec.152, Creating IAM Users

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://gc:p-l1339a@ds153980.mlab.com:53980/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: '[accessKeyId]',
+  secretAccessKey: '[secretAccessKey]'
 };
